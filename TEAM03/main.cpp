dc45777a6484f21d247422bf674c7049dd34ebfe
Your Name <you@example.com>
2022-01-20 14:38:42 +0700

    https://github.com/2021-2022-2-TIN1083-016/KyThuatLapTrinh.git

--- TEAM03/main.cpp
+++ TEAM03/main.cpp
@@ -8,15 +8,11 @@ void Member_02(){
 }
 void Member_03(){
 }
-void Member_04(){
-  
-}
 
 int main(int argc, const char** argv) {
   printf("WELCOME C/C++ - GITHUB\n");
   Member_01();
   Member_02();
   Member_03();
-  Member_04();
   return 0;
 }
