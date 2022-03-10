b1f4d58e5f1986e65ee0eeb370a0e4817894e3e3
HoanMy <hoanmyotk@gmail.com>
2022-03-10 14:10:40 +0700

    my

--- TEAM12/main.cpp
+++ TEAM12/main.cpp
@@ -3,8 +3,6 @@
 void Member_01(){
       printf("Nguyen Hoan My\n");
 }
-void Member_02(){
-}
 void Member_03(){
   printf("hello cac bro,mik la member 4");
 }
@@ -15,7 +13,6 @@ void Member_04(){
 int main(int argc, const char** argv) {
   printf("WELCOME C/C++ - GITHUB\n");
   Member_01();
-  Member_02();
   Member_03();
   Member_04();
   return 0;
