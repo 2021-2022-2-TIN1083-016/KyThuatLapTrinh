b09106427827c7f01d8035108a64496d8952d014
dat1892003 <dat1892003@gmail.com>
2022-03-10 15:16:47 +0700

    a

--- TEAM08/dat/main.cpp
+++ TEAM08/dat/main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include "..\..\_src\Log.h"
 using namespace std;
-main (){
+int main (){
  cout<<"hello";
+  LOG_WT("Logger Warning\n");
+  LOG_IT("Logger Info\n");
+  LOG_ET("Logger Error\n");
+  LOG_DT("Logger Debug\n");
+  LOG_W("Logger Warning\n");
+  LOG_I("Logger Info\n");
+  LOG_E("Logger Error\n");
+  LOG_D("Logger Debug\n");
  return 0;
 }
