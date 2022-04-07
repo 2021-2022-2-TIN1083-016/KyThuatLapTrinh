8802277e7ac0024906354d10dd0c5f01403fa0c4
DuNgocHung <dunghung158@gmail.com>
2022-04-07 14:46:04 +0700

    nhn

--- TEAM11/DU_NGOC_HUNG/kiemtra.cpp
+++ TEAM11/DU_NGOC_HUNG/kiemtra.cpp
@@ -4,7 +4,7 @@
 int main(){
   
   LOG_WT("Bai: Tong so trong xau\n");
-  LOG_IT("Ten: Tran Thanh Dat\n");
+  LOG_IT("Ten: Du NGoc Hung\n");
   LOG_IT("Msv: 21T1080014\n");
   LOG_DT("-----\n");
   LOG_DT("Nhap chuoi:5 random strings contain 234 numbers\n");
