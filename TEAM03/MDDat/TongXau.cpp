b9447443fd996c14bff51bef56f3a15fd06769fc
MAiDucDAt <21t1020880@husc.edu.vn>
2022-04-07 15:28:41 +0700

    mddat

--- TEAM03/MDDat/TongXau.cpp
+++ TEAM03/MDDat/TongXau.cpp
@@ -1,27 +1,30 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+#include <bits/stdc++.h>
 #include "../../_src/Log.h"
-
+using namespace std;
+#define ll long long
+string s;
+ll ans( string &s ){
+    ll res = 0;
+    for( int i = 0; i < s.size(); i++ ){
+        ll ans = 0;
+        while( isdigit(s[i]) ){
+            ans = ans * 10 + s[i] - '0';
+            i++;
+        }
+        res += ans;
+    }
+    return res;
+}
+void Output(){
+    LOG_ET( "Bai : Tong so trong xau\n");
+    LOG_WT("Ho Va Ten: Le Phuoc Quang\n");
+    LOG_IT("Ma sv: 21T1020065\n");
+    LOG_DT("-----\n");
+    LOG_ET( "Nhap chuoi: " ); getline( cin, s );
+    LOG_ET("Tong so: %lld", ans(s));
+}
 int main(){
-	char s[500];
-	printf("Bai: Tong so trong xau\n");
-	printf("Ten: Mai Duc Dat\n");
-	printf("Msv: 21T1020880\n");
-	printf("-----\n");
-	printf("Nhap chuoi:");
-	gets(s);
-	long long cnt = 0, sum = 0;
-	for(int i=0;i<strlen(s);i++){
-		if(s[i]>='0'&&s[i]<='9'){
-			cnt = cnt*10 + s[i] - '0';
-		}
-		else{
-			sum+= cnt;
-			cnt = 0;
-		}
-	}if(s[strlen(s)-1]>='0'&&s[strlen(s)-1]<='9'){
-		sum+=cnt;
-	}
-	printf("Tong so: %lld", sum);
+    ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+    Output();
+    return 0;
 }
