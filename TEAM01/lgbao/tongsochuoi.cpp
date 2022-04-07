a2f39102c6e0ee9cda5503ab4482c28a99e8a870
lebao2003 <21t1020009@husc.edu.vn>
2022-04-07 15:51:30 +0700

    abc

--- TEAM01/lgbao/tongsochuoi.cpp
+++ TEAM01/lgbao/tongsochuoi.cpp
@@ -1,52 +1,32 @@
-#include <iostream>
-#include<string.h>
+#include <bits/stdc++.h>
+#include "../../_src/Log.h"
 using namespace std;
-#include "..\..\_src\Log.h"
 
+int main(){
+    
+    
+    
+   LOG_WT("Bai: Tong so trong xau:\n");
+   LOG_IT("Ten: Le Gia Bao\n");
+   LOG_IT("MSV: 21T1020009\n");
+   LOG_DT("-------------\n");
+   string s;
+   LOG_DT("Nhap chuoi:"); getline(cin, s);
+   
+   string str = "";
+   long long t= 0;
+   for(int i = 0; i <= (int)s.length(); ++i){
+        if(s[i] >= '0' && s[i] <= '9'){
+            str.push_back(s[i]);
+        }
+        else{
+            if(str != ""){
+                t+= stoll(str);
+                str = "";
+            }
+        }
+    } 
 
-void nhap(char S[])
-{
-	gets(S);
-}
-
-void xuat(int x)
-{
-	cout<<x;
-}
-
-int tinhTongSoTrongChuoi(char S[])
-{
-	int len = strlen(S);
-	int i=0;
-	int tong=0;
-	int so=0;
-	while (i<=len)
-	{
-		if (S[i] >= '0' && S[i] <= '9')
-			so = so * 10 + (S[i] - '0');
-		else
-		{
-			tong = tong + so;
-			so = 0;
-		}
-		i++;
-	}
-	return tong;
-}
-
-int main()
-{
-  LOG_WT("Bai: Tong so trong xau\n");
-  LOG_IT("Ten: Le Gia Bao\n");
-  LOG_IT("Msv: 21T1020009\n");
-  LOG_DT("------\n");
-  string s;
-  LOG_DT("Nhap chuoi: ");
-	char S[200];
-	nhap(S);
-	LOG_WT("Tong so: ");
-
-	int tong= tinhTongSoTrongChuoi(S);
-	xuat(tong);
-	return 0;
+   LOG_WT("Tong so: ");  cout<<t;
+  return 0;
 }
