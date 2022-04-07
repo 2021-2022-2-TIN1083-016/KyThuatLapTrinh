f66b7ec91bf6ff32847fe035f5b686082597d5d0
DuNgocHung <dunghung158@gmail.com>
2022-04-07 16:27:37 +0700

    ddd

--- TEAM11/DU_NGOC_HUNG/kiemtra.cpp
+++ TEAM11/DU_NGOC_HUNG/kiemtra.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+/*#include <iostream>
 #include <string>
 
 #include "../../_src/Log.h"
@@ -27,4 +27,32 @@ LOG_WT("Bai: Tong so trong xau\n");
 	getline(cin,a);
 	LOG_WT("Tong so: ");
 	cout<<tongsotrongxau(a)<<endl;	
+}*/
+#include <bits/stdc++.h> 
+#include "../../_src/Log.h"
+using namespace std;
+
+int Tinhsocotrongchuoi (string a,int n){
+	long long d=-1;
+	for(int i=n;isdigit(a[i]);i++) {
+		d++; 
+	}
+	return pow(10,d) ;
+} 
+int main (){
+  LOG_WT("Bai: Tong so trong xau\n");
+    LOG_IT("Ten: Du Ngoc HUng\n");
+    LOG_IT("Msv: 21T1080014\n");
+    LOG_DT("------\n");
+    string a;
+    LOG_DT("Nhap chuoi: ");
+	int tong;
+	getline(cin,a) ;
+	for(int i=0; i< a.size();i++) {
+		if(isdigit(a[i])){
+			tong += (a[i]-'0') *Tinhsocotrongchuoi(a,i) ;
+		} 
+	}
+	LOG_WT("Tong so: ");cout << tong;
+	 
 }
