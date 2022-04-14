6ae4790c5e37a8273004d34ce2798bc699c5278f
trantienloi <trantienloi412@gmail.com>
2022-04-14 04:30:51 +0000

    f

---
+++ TEAM11/tran_tien_loi/th1.cpp
@@ -0,0 +1,31 @@
+#include"stdio.h"
+#include"string.h"
+#include"../../_src/Log.h"
+#include"../../_src/Utils.h"
+void string(char str[]){
+	int i=0,j,sum=0,n; 
+	while(i<=strlen(str)-1) {
+          if(str[i]<='9' && str[i]>='0') 
+           { 
+            j=i; 
+            n=0;            
+            while(j<=strlen(str)-1 && (str[j]<='9' && str[j]>='0')) 
+             { 
+                n=10*n+(int)str[j]-48; 
+                j++; 
+             } 
+             sum+=n;
+             i=j; 
+            }else{
+			 i++; 
+		}
+	}
+	printf("%d",sum);
+}
+
+int main(){
+	char str[501];
+	fflush(stdin);
+	fgets(str, sizeof(str), stdin);
+	string(str);
+}
