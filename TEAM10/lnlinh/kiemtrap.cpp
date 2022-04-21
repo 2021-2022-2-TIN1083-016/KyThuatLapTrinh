79d110091bcffa85b3747a54ce3f6d81ce9d1e45
LeNgoclinh <21t1020476@husc.edu.vn>
2022-04-21 14:21:36 +0700

    đa

--- TEAM10/lnlinh/kiemtrap.cpp
+++ TEAM10/lnlinh/kiemtrap.cpp
@@ -1,44 +1,34 @@
-#include <iostream>
-#include <string.h>
+#include<iostream>
+#include<string.h>
+#include <bits/stdc++.h>
+
 using namespace std;
-	char str[500];
-	int main()
-	{
-		
-  		int count;;
-			cin.getline(str, 500);
-		for (int i = 0; i <= 9; ++i)
-		{	
-			count = 0;
-		for (int j = 0; j < strlen(str); ++j)
-			{
-				if (str[j] == ('0' + i))
-				  count++;
-			}
-		if (count)
-			cout << (char)('0' + i) << " " << count << '\n';
-		}
-		for (int i = 0; i <= 25; ++i)
-		{
-			count = 0;
-		  for (int j = 0; j < strlen(str); ++j)
-			{
-				if (str[j] == ('A' + i))
-				count++;
-			}
-		if (count)
-			cout << (char)('A' + i) << " " << count << '\n';
-		}
-		for (int i = 0; i <= 25; ++i)
-		{
-			count = 0;
-		  for (int j = 0; j < strlen(str); ++j)
-			{
-				if (str[j] == ('a' + i))
-				count++;
-			}
-		if (count)
-			cout << (char)('a' + i) << " " << count << '\n';
-		}
-	return 0;
+
+  char str[500];
+  int main() {
+  int count;;
+  cin.getline(str, 500);
+  for (int i = 0; i <= 9; ++i)
+{
+  count = 0;
+  for (int j = 0; j < strlen(str); ++j)
+{
+  if (str[j] == ('0' + i))
+  count++;
+}
+  if (count)
+  cout << (char)('0' + i) << " " << count << '\n';
+}
+  for (int i = 0; i <= 25; ++i)
+{
+  count = 0;
+  for (int j = 0; j < strlen(str); ++j)
+{
+  if (str[j] == ('a' + i) || str[j] == ('A' + i))
+  count++;
+}
+  if (count)
+  cout << (char)('a' + i) << " " << count << '\n';
+}
+return 0;
 }
