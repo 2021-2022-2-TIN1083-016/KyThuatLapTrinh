6c5d351145ce34ad5e1db693a83a1356ce866827
tranvanhuy <vanhuy26032003@gmail.com>
2022-05-05 15:30:26 +0700

    huy

--- TEAM05/tvhuy/time.cpp
+++ TEAM05/tvhuy/time.cpp
@@ -8,7 +8,7 @@ int main(){
   curl_global_init(CURL_GLOBAL_DEFAULT);
 
   std::string szZone = "japan";
-  std::string url = "http://api.weatherapi.com/v1/search.json?key=&q=japan" + szZone;
+  std::string url = "http://api.weatherapi.com/v1/current.json?key=29ad937ed16a4471a9c70243220505&q=London&aqi=no" + szZone;
   std::string szContent;
   CHttpClient clt;
   if (!clt.SendRequest(url.c_str(),szContent)){
@@ -22,4 +22,4 @@ int main(){
   curl_global_cleanup();
   LOG_D("\n***** EXIT APP*****\n");  
   return 0;
-}
+}// g++ .\time.cpp ..\..\_src\Log.cpp ..\..\_src\HttpClient.cpp -o time.exe -lcurl
