889afca75c5ca05685fadafb50357c051b0bd969
tvhuy <vanhuy26042003@gmail.com>
2022-05-21 12:37:21 +0000

    tvhuy

--- TEAM05/tvhuy/kt2.cpp
+++ TEAM05/tvhuy/kt2.cpp
@@ -1,13 +1,10 @@
-#include <bits/stdc++.h>
-using namespace std;
+
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
 #include "../../_src/Log.h"
 #include "../../_src/Utils.h"
 
-void ktra(char s[]);
-void ktr(char s[], int n);
 int main(){
 
     LOG_D("\n****************\n");
@@ -23,32 +20,25 @@ int main(){
     LOG_IT("msv: %s\n", masv);   
 
 {
-	char s[500];
-	int k;
+
+char s[250];
 	fflush(stdin);
-    LOG_DT("xau Q: ");
-	fgets(s, sizeof(s), stdin);
-    LOG_DT("khoa: ");
+	 LOG_DT("xau Q: ");
+	fgets(s, sizeof(s),stdin);
+	int k;
+	LOG_DT("khoa: ");
 	scanf("%d",&k);
-	ktr(s,k);
+	LOG_DT("xau s: ");
+	for(int i=k-1; i>=0; i--){
+		printf("%c",s[i]);
+	}
+	for(int i=strlen(s)-2; i>=k; i--){
+		printf("%c",s[i]);
+	}
+
+
 }
 
     
 return 0;
-}
-void ktr(char s[], int n){
-	int so = strlen(s)-n-1;
-	char a, m;
-	int s1=strlen(s);
-	for(int i=0; i<n/2; i++){
-		a = s[i];
-		s[i] = s[n-i-1];
-		s[n-i-1] = a;
-	}
-	for(int i=0; i<so/2; i++){
-		m = s[n+i];
-		s[n+i] = s[s1-i-1];
-		s[s1-i-1] = m;
-	}
-	LOG_DT("%s",s);
 }
