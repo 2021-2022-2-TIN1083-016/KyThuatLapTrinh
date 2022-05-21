ae62a0ff2b4b0c4297daf95fc1be1918d3f9f3b0
Your Name <honagchuong050203@example.com>
2022-05-21 12:06:20 +0700

    nvhc

---
+++ TEAM07/NVHC/BaiTapp.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<math.h>
+#include "..\..\_src\Log.h"
+#include "..\..\_src\Utils.h"
+
+using namespace std;
+
+int main(){
+    int x , y ;
+    x = 12 ;
+    y = 13 ;
+    cout << x + y << endl ;
+    return 0;
+}
