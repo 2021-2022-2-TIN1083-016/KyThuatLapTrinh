89fda9d6bb6f212d35a433e76189826e750d1b88
NguyenLuuDucMinh <nguyenluuducminhvta2017@gmail.com>
2022-05-21 16:35:28 +0700

    minh

---
+++ TEAM12/Minh/thi.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+#include"../../_src/Log.h"
+int main(){
+    LOG_WT("Bai: Ma hoa van ban\n");
+    LOG_IT("Ten: Nguyen Luu Duc Minh\n");
+    LOG_IT("Msv: 21T1020518\n");
+    string s1;
+	int k;
+    LOG_IT("Xau Q:");
+	getline(cin,s1);
+    LOG_WT("Khoa K:");
+	cin>>k;	
+	string s2=s1.substr(0,k);
+	string s3=s1.substr(k);
+    LOG_DT("Xau S:");
+	for(int i=s2.length()-1;i>=0;i--){
+		cout<<s2[i];
+	}
+	for(int i=s3.length()-1;i>=0;i--){
+		cout<<s3[i];
+	}
+}
